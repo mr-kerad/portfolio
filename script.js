5f68220f17e4a6fd322a5632387d9fbdbe348ebd
MR-KERAD <131299648+mr-kerad@users.noreply.github.com>
2024-03-30 07:26:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const observer = new IntersectionObserver((entries) => {
           entry.target.classList.remove('look');
       }
   });
-}, { threshold: 0.4 }); // Set the threshold to 0.35
+}, { threshold: 0.35 }); // Set the threshold to 0.35
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
@@ -63,3 +63,51 @@ document.getElementById('faq-item-5').addEventListener('click', function() {
   }
 });
 */
+
+/*Mobile typetext in about section*/
+// set up text to print, each item in array is new line
+var aText = new Array(
+  "There are only 10 types of people in the world:",
+  "Those who understand binary, and those who don't."
+);
+var iSpeed = 100; // time delay of print out
+var iIndex = 0; // start printing array at this position
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+
+function typewriter() {
+  sContents = ' ';
+  iRow = Math.max(0, iIndex - iScrollAt);
+  var destination = document.getElementById("typedtext");
+
+  while (iRow < iIndex) {
+      sContents += aText[iRow++] + '<br />';
+  }
+  destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+  if (iTextPos++ == iArrLength) {
+      iTextPos = 0;
+      iIndex++;
+      if (iIndex == aText.length) {
+          setTimeout(function() {
+              iIndex = 0; // Reset iIndex to 0 after 5 seconds
+              typewriter();
+          }, 5000);
+          return; // Exit the function to prevent immediate repetition
+      }
+      iArrLength = aText[iIndex].length;
+      setTimeout("typewriter()", 500);
+  } else {
+      setTimeout("typewriter()", iSpeed);
+  }
+}
+
+typewriter();
+
+
+  
+  
+  typewriter();
